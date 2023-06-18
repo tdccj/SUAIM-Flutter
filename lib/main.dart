d7a132ca1d4558d94677e3789c76d287207b46dc
tdccj <tudouccj@163.com>
2023-06-18 20:32:56 +0800

    设置默认字体为harmonyos成功

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+      theme: ThemeData(fontFamily: 'HarmonyOS_Sans_SC'),
       home: Scaffold(
-    appBar: AppBar(
-      title: const Text("溯物-SUAIM",
-          style: TextStyle(
-              color: Color.fromARGB(255, 24, 121, 201),
-              fontWeight: FontWeight.w300)), // 设置标题颜色
-      // centerTitle: true, // 设置标题居中
-      backgroundColor: Colors.white, // 设置appbar背景
-      elevation: 2, // 阴影
-    ),
-    body: const MyApp(),
-  )));
+        appBar: AppBar(
+          title: const Text("溯物-SUAIM",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 24, 121, 201),
+                  fontWeight: FontWeight.w400)), // 设置标题颜色
+          // centerTitle: true, // 设置标题居中
+          backgroundColor: Colors.white, // 设置appbar背景
+          elevation: 2, // 阴影
+        ),
+        body: const MyApp(),
+      )));
 }
 
 class MyApp extends StatelessWidget {
