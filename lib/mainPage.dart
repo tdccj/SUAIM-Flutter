2bccd8a75966fda65dc76efc2427f4d84805b887
tdccj <tudouccj@163.com>
2023-07-03 14:07:52 +0800

    微调，添加注释

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -3,16 +3,15 @@ import 'package:flutter/animation.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key}); // 定义构造方法
-  void _onButtonpressed() {
-    print('object');
-  }
 
   @override
   Widget build(BuildContext context) {
     // 构建MyApp组件
     return Container(
+      // 创建主页面
       color: const Color.fromARGB(255, 255, 255, 255),
       child: Column(
+        // 第一层结构为cloumn，分割显示和按钮
         children: [
           const SizedBox(
             height: 30,
@@ -20,6 +19,7 @@ class MainPage extends StatelessWidget {
           Flexible(
             flex: 4,
             child: Row(
+              // 显示数据
               children: [
                 const SizedBox(
                   width: 30,
@@ -36,9 +36,11 @@ class MainPage extends StatelessWidget {
             ),
           ),
           const Spacer(
+            // 分割两大模块
             flex: 1,
           ),
           const SizedBox(
+            // 功能按钮区
             height: 150,
           )
         ],
